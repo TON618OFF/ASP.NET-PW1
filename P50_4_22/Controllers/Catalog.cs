040c2b605910bf261aeef5d4e7d3efde9f3db40f
TON618OFF <rusblackgod@gmail.com>
2024-09-22 10:36:07 +0300

    Добавьте файлы проекта.

---
+++ P50_4_22/Controllers/Catalog.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace P50_4_22.Controllers
+{
+    public class Catalog : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ P50_4_22/Controllers/SupportController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace P50_4_22.Controllers
+{
+    public class SupportController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
